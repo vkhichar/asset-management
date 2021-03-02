827a3e2411e5e327b5d9541d3174b25671f10eb3
Fatema Moaiyadi <fatema.moaiyadi@joshsoftware.com>
2021-03-02 18:40:47 +0530

    Seeds DB with relevant data-updated

--- seed.sh
+++ seed.sh
@@ -9,14 +9,14 @@ insert into users (name,email,password,is_admin) values ('Alisa Ray','alisaray@g
 insert into users (name,email,password,is_admin) values ('Tom Walters','tomwalters@gmail.com','tom123',false);
 insert into users (name,email,password,is_admin) values ('Alice Stephen','alicestep@gmail.com','alice776',true);
 
-insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('Active','Laptop','01/07/2017',50000,'Dell Latitude E5550','{"RAM":"4GB","HDD":"500GB","Generation":"i5"}');
-insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('Maintenance','Laptop','01/10/2017',40000,'IBM Thinkpad','{"RAM":"4GB","HDD":"500GB","Generation":"i5"}');
-insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('Active','Laptop','10/05/2016',45000,'Lenovo Gaming Pad','{"RAM":"8GB","HDD":"500GB","Generation":"i8"}');
-insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('Active','Laptop','10/10/2015',60000,'MacBook','{"RAM":"4GB","HDD":"500GB","Colour":"Space Grey"}');
-insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('Active','Mouse','11/08/2019',500,'Intel','{"Wireless":"Yes","Sensor":"Laser"}');
-insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('Not Active','Laptop','29/12/2016',450,'Logitech','{"Wireless":"No","Sensor":"Laser"}');
-insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('Active','Keyboard','26/07/2019',750,'HP','{"Height":"34 mm","Width":"13 cms"}');
-insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('Active','Earphones','22/05/2020',400,'Boat','{"Color":"Black"}');
+insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('active','Laptop','01/07/2017',50000,'Dell Latitude E5550','{"RAM":"4GB","HDD":"500GB","Generation":"i5"}');
+insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('undermaintenance','Laptop','01/10/2017',40000,'IBM Thinkpad','{"RAM":"4GB","HDD":"500GB","Generation":"i5"}');
+insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('active','Laptop','10/05/2016',45000,'Lenovo Gaming Pad','{"RAM":"8GB","HDD":"500GB","Generation":"i8"}');
+insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('active','Laptop','10/10/2015',60000,'MacBook','{"RAM":"4GB","HDD":"500GB","Colour":"Space Grey"}');
+insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('active','Mouse','11/08/2019',500,'Intel','{"Wireless":"Yes","Sensor":"Laser"}');
+insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('retired','Laptop','29/12/2016',450,'Logitech','{"Wireless":"No","Sensor":"Laser"}');
+insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('active','Keyboard','26/07/2019',750,'HP','{"Height":"34 mm","Width":"13 cms"}');
+insert into assets (status,category,purchase_at,purchase_cost,name,specifications) values ('active','Earphones','22/05/2020',400,'Boat','{"Color":"Black"}');
 
 insert into asset_allocations (user_id,asset_id,allocated_by,allocated_from) values (2,'e9c1af12-2f15-478a-ac36-e14071f69b10',1,'02/07/2017');
 insert into asset_allocations (user_id,asset_id,allocated_by,allocated_from,allocated_till) values (2,'e9c1af12-3f15-478a-ac36-e14071f69b10',4,'02/07/2017','07/10/2019');
