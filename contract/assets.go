c65430b195863dd666da0236df6aaf5d394d5e74
Mohit Dehuliya <mohitdehuliya@joshsoftware.com>
2021-03-04 20:01:48 +0530

    UPDATED ASSET MASTER R

--- contract/assets.go
+++ contract/assets.go
@@ -20,6 +20,7 @@ type Asset struct {
 }
 
 func DomainToContractassets(d *domain.Asset) Asset {
+
 	u := Asset{
 		Id:             d.Id,
 		Status:         d.Status,
--- handler/asset_handler.go
+++ handler/asset_handler.go
@@ -14,45 +14,23 @@ func ListAssetHandler(asset service.AssetService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		asset, err := asset.ListAssets(r.Context())
-
-		if err == customerrors.NoAssetsExist {
-			fmt.Println("handler: No assets exist")
-
-			w.WriteHeader(http.StatusNotFound)
-			responseBytes, err := json.Marshal(contract.ErrorResponse{Error: "no asset found"})
-			if err != nil {
-				fmt.Printf("handler: Something went wrong while Marshaling: %s", err.Error())
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Write(responseBytes)
-			return
-		}
-
-		asset, err = asset.ListAssets(r.Context())
-
-		if err != nil {
-
-			fmt.Printf("handler:Error while Searching for Assets, %s", err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
 		if err == customerrors.NoAssetsExist {
 			fmt.Println("handler: No assets exist")
 			w.WriteHeader(http.StatusNotFound)
 			responseBytes, _ := json.Marshal(contract.ErrorResponse{Error: "no asset found"})
 			w.Write(responseBytes)
 			return
-
 		}
-
+		if err != nil {
+			fmt.Printf("handler:Error while Searching for Assets, %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		w.WriteHeader(http.StatusOK)
 		assetResp := make([]contract.Asset, 0)
 		for _, u := range asset {
 			assetResp = append(assetResp, contract.DomainToContractassets(&u))
 		}
-
 		responseBytes, err := json.Marshal(assetResp)
 
 		if err != nil {
