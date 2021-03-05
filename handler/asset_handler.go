9502b73803c132a07b952dd60e85c18d54946364
Mohit Dehuliya <Mohit Dehuliya>
2021-03-05 12:43:44 +0530

    ALL CHANGES DONE IN ASSET_MASTER

--- handler/asset_handler.go
+++ handler/asset_handler.go
@@ -21,6 +21,7 @@ func ListAssetHandler(asset service.AssetService) http.HandlerFunc {
 			w.WriteHeader(http.StatusNotFound)
 			responseBytes, err := json.Marshal(contract.ErrorResponse{Error: "no asset found"})
 			if err != nil {
+
 				fmt.Printf("handler: Something went wrong while Marshaling: %s", err.Error())
 				w.WriteHeader(http.StatusInternalServerError)
 				return
@@ -38,8 +39,10 @@ func ListAssetHandler(asset service.AssetService) http.HandlerFunc {
 		for _, u := range asset {
 			assetResp = append(assetResp, contract.DomainToContractassets(&u))
 		}
+
 		responseBytes, err := json.Marshal(assetResp)
 		if err != nil {
+
 			fmt.Printf("handler: Something Went Wrong while Marshaling assets: %s", err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
 			return
