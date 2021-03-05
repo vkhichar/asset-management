d3a94e5c770d21a23c60bac1c37588283f74dc45
Vikash Khichar <vikash.khichar@joshsoftware.com>
2021-03-05 15:56:10 +0530

    Fix print statement

--- handler/asset_handler.go
+++ handler/asset_handler.go
@@ -22,7 +22,10 @@ func ListAssetHandler(asset service.AssetService) http.HandlerFunc {
 			responseBytes, err := json.Marshal(contract.ErrorResponse{Error: "no asset found"})
 			if err != nil {
 				fmt.Printf("handler: Something went wrong while Marshaling: %s", err.Error())
+<<<<<<< HEAD
 
+=======
+>>>>>>> Fix print statement
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
