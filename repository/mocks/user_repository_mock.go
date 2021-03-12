cdfe4796d2799b0df463049e0ab3a080b35649a0
Gourav Jondhale <gourav.jondhale@joshsoftware.com>
2021-03-12 10:41:29 +0530

    adds get user by id api adds tests for create user api and get user by id api on all layers

--- repository/mocks/user_repository_mock.go
+++ repository/mocks/user_repository_mock.go
@@ -1,70 +1,70 @@
 package mocks
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	"github.com/stretchr/testify/mock"
-	"github.com/vkhichar/asset-management/customerrors"
-	"github.com/vkhichar/asset-management/domain"
-)
+// 	"github.com/stretchr/testify/mock"
+// 	"github.com/vkhichar/asset-management/customerrors"
+// 	"github.com/vkhichar/asset-management/domain"
+// )
 
-type MockUserRepo struct {
-	mock.Mock
-}
+// type MockUserRepo struct {
+// 	mock.Mock
+// }
 
-func (m *MockUserRepo) FindUser(ctx context.Context, email string) (*domain.User, error) {
-	args := m.Called(ctx, email)
+// func (m *MockUserRepo) FindUser(ctx context.Context, email string) (*domain.User, error) {
+// 	args := m.Called(ctx, email)
 
-	var user *domain.User
-	if args[0] != nil {
-		user = args[0].(*domain.User)
-	}
+// 	var user *domain.User
+// 	if args[0] != nil {
+// 		user = args[0].(*domain.User)
+// 	}
 
-	var err error
-	if args[1] != nil {
-		err = args[1].(error)
-	}
+// 	var err error
+// 	if args[1] != nil {
+// 		err = args[1].(error)
+// 	}
 
-	return user, err
-}
+// 	return user, err
+// }
 
-func (m *MockUserRepo) CreateUser(ctx context.Context, user domain.User) (*domain.User, error) {
+// func (m *MockUserRepo) CreateUser(ctx context.Context, user domain.User) (*domain.User, error) {
 
-	args := m.Called(ctx, user)
+// 	args := m.Called(ctx, user)
 
-	var newUser *domain.User
-	if args[0] != nil {
-		newUser = args[0].(*domain.User)
-	}
+// 	var newUser *domain.User
+// 	if args[0] != nil {
+// 		newUser = args[0].(*domain.User)
+// 	}
 
-	var err error
-	if args[1] != nil {
-		err = args[1].(error)
-	}
-	return newUser, err
-}
+// 	var err error
+// 	if args[1] != nil {
+// 		err = args[1].(error)
+// 	}
+// 	return newUser, err
+// }
 
-func (m *MockUserRepo) ListUsers(ctx context.Context) ([]domain.User, error) {
-	// TODO: define mock method
-	return nil, nil
-}
+// func (m *MockUserRepo) ListUsers(ctx context.Context) ([]domain.User, error) {
+// 	// TODO: define mock method
+// 	return nil, nil
+// }
 
-func (m *MockUserRepo) GetUserByID(ctx context.Context, ID int) (*domain.User, error) {
+// func (m *MockUserRepo) GetUserByID(ctx context.Context, ID int) (*domain.User, error) {
 
-	var newUser *domain.User
-	args := m.Called(ctx, ID)
+// 	var newUser *domain.User
+// 	args := m.Called(ctx, ID)
 
-	if args[0] == nil && args[1] == nil {
-		return nil, customerrors.UserNotExist
-	}
-	if args[0] != nil {
-		newUser = args[0].(*domain.User)
-	}
+// 	if args[0] == nil && args[1] == nil {
+// 		return nil, customerrors.UserNotExist
+// 	}
+// 	if args[0] != nil {
+// 		newUser = args[0].(*domain.User)
+// 	}
 
-	var err error
+// 	var err error
 
-	if args[1] != nil {
-		err = args[1].(error)
-	}
-	return newUser, err
-}
+// 	if args[1] != nil {
+// 		err = args[1].(error)
+// 	}
+// 	return newUser, err
+// }
